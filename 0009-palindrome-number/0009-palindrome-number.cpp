fda604e6611786dda1ec1f3c1f5142ebfc599991
influx <71868156+Abhi-187@users.noreply.github.com>
2022-11-05 03:09:49 -0700

    Time: 23 ms (69.28%), Space: 5.9 MB (31.61%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int n) {
         
-       
-        if(n < 0 || (n % 10 == 0 && n != 0)) {
-            return false;
+       string str = to_string(n);
+       int len = str.length();
+        for (int i = 0; i< len; i++){
+            if(str[i] != str[len-i-1]){
+                return false;
+            }
         }
-        
-        
-        int revnum = 0;
-        while(n > revnum) {
-            revnum = (revnum * 10) + (n % 10);
-            n = n / 10;
-        }
-      
-        return n == revnum || n == revnum / 10;
-        
-    }
-};
+        return true;
+}};
