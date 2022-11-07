8472310278ed4e3118750b874a3b848eac88b031
influx <71868156+Abhi-187@users.noreply.github.com>
2022-11-07 23:33:17 +0530

    Time: 11 ms (39.98%), Space: 9.2 MB (81.89%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& S) {
+         if (S.size() == 0) return "";
+    for (int i = 0; i < S[0].length() ; i++){
+        char c = S[0][i];
+        for (int j = 1; j < S.size(); j++) {
+            if (i == S[j].length() || S[j][i] != c)
+                return S[0].substr(0, i);             
+        }
+    }
+    return S[0];
+
+        
+    }
+};
